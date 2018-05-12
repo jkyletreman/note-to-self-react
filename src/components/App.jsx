b3e7df437904a06f74284c3253459218178586c9
Kyle Treman <jkt290@nyu.edu>
2018-05-12 19:35:39 -0400

    Add form control, onchange, onclick events

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-import React, { Component } from 'react';
-import { Form, FormControl, Button } from 'react-bootstrap';
+import React, { Component } from "react";
+import { Form, FormControl, Button } from "react-bootstrap";
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      test: ""
+    };
+  }
+
   render() {
     return (
       <div>
         <h2>Not to Self</h2>
         <Form>
-          <FormControl />
-          <Button>Submit</Button>
+          <FormControl
+            onChange={e => {
+              this.setState({ test: e.target.value });
+            }}
+          />
+          <Button onClick={() => console.log(this.state)}>Submit</Button>
         </Form>
       </div>
-    )
+    );
   }
 }
