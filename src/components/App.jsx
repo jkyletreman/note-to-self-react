c085d24585c45c9fba4fa55def1a55ffbb290ec5
Kyle Treman <jkt290@nyu.edu>
2018-05-12 19:30:02 -0400

    Add form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { Form, FormControl, Button } from 'react-bootstrap';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <h2>Not to Self</h2>
+        <Form>
+          <FormControl />
+          <Button>Submit</Button>
+        </Form>
       </div>
     )
   }
