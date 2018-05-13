d17c6c7ee1d869c1d0be144cbbcb646918e2b549
Kyle Treman <jkt290@nyu.edu>
2018-05-12 21:06:18 -0400

    Fix import of adapter

--- src/components/Note.test.js
+++ src/components/Note.test.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import Note from './Note';
+import "../setupTests"
 
 describe("Note", () => {
-  let note = mount(<Note />);
+  let note = mount(<Note note={{ text: 'test note' }}/>);
+
+  it("renders the note text", () => {
+    console.log(note.debug())
+  })
 })
