b7966bbc1ead1c3b861cae44b2c19c28a39eafc0
Kyle Treman <jkt290@nyu.edu>
2018-05-12 21:09:18 -0400

    Add test for rendering text

--- src/components/Note.test.js
+++ src/components/Note.test.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import Note from './Note';
-import "../setupTests"
+import "../setupTests";
+
+const props = { note: { text: 'test note' }}
 
 describe("Note", () => {
-  let note = mount(<Note note={{ text: 'test note' }}/>);
+  let note = mount(<Note {...props} />);
 
   it("renders the note text", () => {
+    // displays the node as a string
     console.log(note.debug())
+    expect(note.find('p').text()).toEqual(props.note.text)
   })
 })
