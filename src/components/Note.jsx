b599be8269bb45e858b527ada2a1874ac4739662
Kyle Treman <jkt290@nyu.edu>
2018-05-12 20:16:30 -0400

    Add styling class

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Note extends Component {
   render() {
     return (
-      <div>
+      <div className="note">
         <p>{this.props.note.text}</p>
       </div>
     )
